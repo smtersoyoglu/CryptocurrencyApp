0b666e35410dad7e8dd1bfbe6cdd16d04ebe204b
smter <smtersoyoglu231@gmail.com>
2025-01-19 18:38:25 +0300

    Refactor project structure, add dependencies, and implement Hilt
    -   Moved `MainActivity.kt` to `presentation` package.
    -   Moved `CryptocurrencyAppTheme` to `presentation/theme`.
    -   Created `Constants.kt` for storing constant values.
    -   Created `Resource.kt` to handle API response.
    -   Created `CoinApplication.kt` for application initialization with Hilt.
    -   Updated `libs.versions.toml` to include dependencies such as `hilt`, `coil`, `navigation-compose`, `logging-interceptor`, `okhttp`, `retrofit`, `converter-gson`, `lifecycle-process`, `hilt-common`, `hilt-work`, `serialization-json`, and `kotlinx-coroutines`.
    -   Added Hilt and KSP plugin to `libs.versions.toml`.
    -   Added `hilt`, `ksp`, and `serialization` plugins to `build.gradle.kts`.
    - Added `hilt.android`, `hilt.compiler`, `androidx.hilt.navigation.compose`, `kotlinx.coroutines.core`, `kotlinx.coroutines.android`, `coil.compose`, `serialization.json`, `retrofit`, `converter.gson`, `okhttp.v500alpha2`, and `logging.interceptor` dependencies to `app/build.gradle.kts`.

---
+++ app/src/main/java/com/smtersoyoglu/cryptocurrencyapp/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.smtersoyoglu.cryptocurrencyapp.common
+
+object Constants {
+
+    const val BASE_URL = "https://api.coinpaprika.com/"
+}
--- app/src/main/java/com/smtersoyoglu/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/smtersoyoglu/cryptocurrencyapp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.smtersoyoglu.cryptocurrencyapp
+package com.smtersoyoglu.cryptocurrencyapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.smtersoyoglu.cryptocurrencyapp.ui.theme.CryptocurrencyAppTheme
+import com.smtersoyoglu.cryptocurrencyapp.presentation.theme.CryptocurrencyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
