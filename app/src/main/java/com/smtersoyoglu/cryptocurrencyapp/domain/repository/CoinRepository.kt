02c6c75cbf158f16153dc735e8aa0bcbe613c74e
smter <smtersoyoglu231@gmail.com>
2025-01-21 19:46:31 +0300

    Refactor Coin Detail Use Case and Repository with Flow
    -   Updated `GetCoinDetailByIdUseCase` to return `Flow<Resource<CoinDetail>>` instead of `Resource<CoinDetail>`.
    -   Updated `CoinRepository` to have `getCoinById` return `Flow<Resource<CoinDetail>>` instead of `suspend Resource<CoinDetail>`.
    -   Updated `CoinRepositoryImpl` to use `safeFlow` for `getCoinById`, making it return `Flow<Resource<CoinDetail>>`.
    -  Updated `getCoins` function to use `safeFlow` function for return `Flow`.

--- app/src/main/java/com/smtersoyoglu/cryptocurrencyapp/domain/repository/CoinRepository.kt
+++ app/src/main/java/com/smtersoyoglu/cryptocurrencyapp/domain/repository/CoinRepository.kt
@@ -9,6 +9,6 @@ interface CoinRepository {
 
     fun getCoins(): Flow<Resource<List<Coin>>>
 
-    suspend fun getCoinById(coinId: String): Resource<CoinDetail>
+    fun getCoinById(coinId: String): Flow<Resource<CoinDetail>>
 
 }
--- app/src/main/java/com/smtersoyoglu/cryptocurrencyapp/domain/use_case/coin_detail/GetCoinDetailByIdUseCase.kt
+++ app/src/main/java/com/smtersoyoglu/cryptocurrencyapp/domain/use_case/coin_detail/GetCoinDetailByIdUseCase.kt
@@ -3,12 +3,13 @@ package com.smtersoyoglu.cryptocurrencyapp.domain.use_case.coin_detail
 import com.smtersoyoglu.cryptocurrencyapp.common.Resource
 import com.smtersoyoglu.cryptocurrencyapp.domain.model.CoinDetail
 import com.smtersoyoglu.cryptocurrencyapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetCoinDetailByIdUseCase @Inject constructor(
     private val coinRepository: CoinRepository,
 ) {
-    suspend operator fun invoke(coinId: String): Resource<CoinDetail> {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> {
         return coinRepository.getCoinById(coinId)
     }
 }
